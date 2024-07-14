4c8d1a51c66bab97a35b21e434d78132d4562596
GlitchyStar717 <cr7keshu@gmail.com>
2024-07-14 12:54:03 +0545

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Git & GitHub Workshop - Demo for Github Pages & Github Actions
+
+UI Forked from : https://github.com/Rahul1582/portfolio-rahulkp.git
---
+++ src/data.js
@@ -0,0 +1,16 @@
+const data = {
+    name: "GlitchyStar717", //Your GitHub Username
+    githubLink: "https://github.com/GlitchyStar717" , //Your GitHub Profile link
+    email : "cr7keshu@gmail.com", //Your GitHub email address
+    hobbies : [
+        "Movies",
+        "Travelling",
+        "Football"
+    ],
+    jobTitle : "Computer Engineer",
+    degree: "Bachelor's in Computer Engineering",
+    campus: "Pulchowk Campus",
+    location: "Nepal",
+    spokenLanguages: "Nepali, English, Hindi, German",
+}
+export default data;
